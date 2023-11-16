e9474f3fb24c006655aa35bcaa94f914d11e29cb
Didier Oliva <di.oliva@itecriocuarto.org.ar>
2023-11-16 14:58:01 -0300

    jquery

---
+++ js/ajax.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+    $("#datospersonales").click(function(){
+        $("#content-description").load("datospersonales.html");
+    });
+    
+    $("#experiencia").click(function(){
+        $("#content-description").load("experiencia.html");
+    });
+    
+    $("#educacion").click(function(){
+        $("#content-description").load("educacion.html");
+    });
+    
+    $("#referencias").click(function(){
+        $("#content-description").load("referencias.html");
+    });
+
+    $("#contacto").click(function(){
+        $("#content-description").load("contacto.html");
+    });
+
+    $("#educacion").click(function(){
+        $("#content-description").load("educacion.html");
+    });
+
+    
+});
